0bc6d466a10c13e4922a298aa02f60708063f768
RetropolisRepo <CaldonBowden@outlook.com>
2025-07-03 14:35:17 -0400

    Item Price Changes
    Reduced Ultraball buy/sell prices from 1200/600 to 800/400
    Reduced Greatball buy/sell prices from 600/300 to 500/250
    
    (More mons, more demand. Also getting ahead of offsetting the increase in TM prices, which would make money extra tight early-game)

--- data/itemdata/itemdata.c
+++ data/itemdata/itemdata.c
@@ -143,7 +143,7 @@ const ITEMDATA __data[] =
 
 [ITEM_ULTRA_BALL] =
 {
-    .price = 1200,
+    .price = 800,
     .holdEffect = 0,
     .holdEffectParam = 0,
     .pluckEffect = 0,
@@ -208,7 +208,7 @@ const ITEMDATA __data[] =
 
 [ITEM_GREAT_BALL] =
 {
-    .price = 600,
+    .price = 500,
     .holdEffect = 0,
     .holdEffectParam = 0,
     .pluckEffect = 0,
